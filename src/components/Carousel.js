6c8fdd48646807061f81558b0b301cff931f73c1
Sharjeel Hussain <sharjeelhussain877@gmail.com>
2024-06-15 13:55:04 +0500

    git push

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -17,7 +17,10 @@ const theme = {
 
 export default function HomeCarousel() {
   return (
-    <Carousel className="max-w-screen-3xl rounded-md" {...theme.carousel.defaultProps}>
+    <Carousel
+      className="max-w-screen-3xl rounded-md"
+      {...theme.carousel.defaultProps}
+    >
       <div className="relative w-full h-[100vh] md:h-auto">
         <img
           src="https://images.unsplash.com/photo-1571171637578-41bc2dd41cd2?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
@@ -38,7 +41,9 @@ export default function HomeCarousel() {
               color="white"
               className="mb-12 opacity-80 nunito"
             >
-              When I write code, I focus on clarity, efficiency, and maintainability. I start by thoroughly understanding the problem and planning the solution. I follow best practices and write clean, well-documented code that is easy to read and understand. I prioritize modularity, breaking down complex problems into smaller, manageable components. I also ensure my code is optimized for performance and scalability. Testing is a crucial part of my process to catch and fix issues early. Finally, I constantly review and refactor my code to improve its quality and maintainability.
+              I prioritize clear, efficient code through thorough planning and
+              best practices, emphasizing modularity, performance, testing, and
+              continual refinement for quality and maintainability.
             </Typography>
             {/* <div className="flex justify-center gap-2">
               <Button size="lg" color="white">
@@ -71,7 +76,10 @@ export default function HomeCarousel() {
               color="white"
               className="mb-12 opacity-80 nunito"
             >
-              When I design user interfaces, I prioritize creating intuitive, visually appealing, and user-friendly experiences. I start by understanding the user's needs and project requirements, then sketch out wireframes and mockups to establish a clear design flow. I ensure consistency and usability by following design principles and best practices. Utilizing frameworks like Tailwind CSS and Material-UI, I create responsive and accessible designs that work seamlessly across devices. I value feedback and involve stakeholders early in the process to refine and enhance the design, aiming for interfaces that are both beautiful and functional.
+              I prioritize intuitive, visually appealing user interfaces by
+              understanding needs, creating clear designs with frameworks like
+              Tailwind CSS and Material-UI. I ensure consistency, usability, and
+              stakeholder feedback for beautiful, functional interfaces.
             </Typography>
             {/* <div className="flex justify-center gap-2">
               <Button size="lg" color="white">
@@ -98,14 +106,14 @@ export default function HomeCarousel() {
               className="mb-4 text-3xl md:text-4xl lg:text-5xl nunito"
             >
               How I Plan to Design Applications
-
             </Typography>
             <Typography
               variant="lead"
               color="white"
               className="mb-12 opacity-80 nunito"
             >
-              When designing applications, I start by understanding the project's goals and user needs through thorough research and stakeholder discussions. I create a detailed plan with user personas, user journeys, and feature lists to guide the design. I then develop wireframes and prototypes to visualize the layout and flow, prioritizing usability, consistency, and accessibility. I use modern design tools and frameworks to ensure the interfaces are responsive and visually appealing. I involve stakeholders and users early in the process to gather feedback and make iterative improvements, aiming to deliver an application that provides an exceptional user experience.
+              
+I begin by thoroughly researching project goals and user needs, creating detailed plans and prototypes for intuitive, accessible designs. Utilizing modern tools and stakeholder feedback, I iterate to deliver exceptional user experiences.
             </Typography>
             {/* <div className="flex justify-center gap-2">
               <Button size="lg" color="white">
