cab70425dcffc9b6644ca9a1b3dbcc0e4065739b
SharjeelHussain877 <sharjeelhussain877@gmail.com>
2024-07-06 23:21:17 -0700

    new

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,14 +6,16 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata = {
   title: "𝙎𝙝𝙖𝙧𝙟𝙚𝙚𝙡 𝙃𝙪𝙨𝙨𝙖𝙞𝙣- 𝙈𝙚𝙧𝙣 𝙨𝙩𝙖𝙘𝙠 𝘿𝙚𝙫𝙚𝙡𝙤𝙥𝙚𝙧",
   description: "Open source portfolio for biggners!",
+  icon: '/avatar.png',
 };
+
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
       <meta name="description" content={metadata.description} />
-      <link rel="icon" href="/avatar.png" />
+      <link rel="icon" href={metadata.icon} />
         <script
           src="https://kit.fontawesome.com/3b2468ae5a.js"
           crossOrigin="anonymous"
